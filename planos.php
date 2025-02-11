0e0014ab13bec04afa8c5ed44be92963e08e3a2a
Pedro <pedrottorres07@gmail.com>
2025-02-11 14:05:01 -0300

    Adicionando planos

---
+++ planos.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="Css/styles.css">
+    <title>Pedro Hospedagem Hostinger</title>
+    <link rel="shortcut icon" href="Img/servico-de-ti.png" type="image/x-icon">
+</head>
+<body>
+<?php include ("header.php")?>
+
+
+        <h4 class="main-title">Escolha seu Plano</h4>
+                <div class="corpo">
+        <section class="pricing">
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Essencial</h2>
+                <p><span class="pricing-plan__price">R&#36;7.95</span> / mês</p>
+                
+                <ul class="pricing-plan__features">
+                    <li>1 Website</li>
+                    <li>1Gb de Armazenamento</li>
+                    <li>10Gb de transfêrencia</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta" alt="Assinar plano">Assine Já</a>
+            </div>
+
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Premium</h2>
+                <p><span class="pricing-plan__price">R&#36;10.95</span> / mês</p>
+                <ul class="pricing-plan__features">
+                    <li>5 Websites</li>
+                    <li>3Gb de Armazenamento</li>
+                    <li>Transfêrencia ilimitada</li>
+                    <li>10 horas de suporte mensais</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta" alt="Assinar plano">Assine Já</a>
+            </div>
+
+            <div class="pricing-plan">
+                <h2 class="pricing-plan__title">Gold</h2>
+                <p><span class="pricing-plan__price">R&#36;13.95</span> / mês</p>
+                <ul class="pricing-plan__features">
+                    <li>10 Websites</li>
+                    <li>30Gb de Armazenamento</li>
+                    <li>Transfêrencia ilimitada</li>
+                    <li>30 horas de suporte mensais</li>
+                    <li>Email personalizado</li>
+                </ul>
+                <a href="#" class="pricing-plan__cta" alt="Assinar plano">Assine Já</a>
+            </div>
+        </section>
+    </div>
+       <?php include "footer.php" ?>
+</body>
+</html>
